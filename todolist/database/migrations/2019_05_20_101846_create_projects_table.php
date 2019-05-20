7f16e87f3c9a48dbda28aed3f2d9867c8c30df4b
Gygram <40740085+Gygram@users.noreply.github.com>
2019-05-20 12:28:54 +0200

    Projects and types tables & models

---
+++ todolist/database/migrations/2019_05_20_101846_create_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->uuid('id_project');
+            $table->string('title', 32);
+            $table->text('description');
+            $table->integer('fk_type')->unsigned()->index();
+            $table->timestamps();
+        });
+
+        Schema::table('projects', function (Blueprint $table) {
+            $table->foreign('fk_type')->references('id_type')->on('types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
