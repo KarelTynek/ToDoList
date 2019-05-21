3e9a7176272f7b55027502e9614b123b52c40599
Gygram <40740085+Gygram@users.noreply.github.com>
2019-05-21 12:59:08 +0200

    Update profile.blade.php

--- todolist/resources/views/profile.blade.php
+++ todolist/resources/views/profile.blade.php
@@ -3,27 +3,42 @@
 @section('content')
 <div class="container">
     <div class="row mt-2">
-        <a href="{{ route('createproject') }}" type="button" class="btn btn-primary ml-3">
-            Nový projekt
-        </button></a>
+        <div class="col-3">
+            <form method="GET" action="">
+                <div class="form-group">
+                    <select name="sort" class="form-control">
+                        <option disabled selected>Řadit podle</option>
+                        <option value="name">Název</option>
+                        <option value="date">Datum</option>
+                        <option value="type">Typ</option>
+                    </select>
+                </div>
+            </form>
+        </div>
+        <div class="col">
+            <a href="{{ route('createproject') }}" type="button" class="btn btn-primary">
+                Nový projekt
+            </a>
+        </div>
     </div>
-    <div class="row mt-1">
+    <div class="row">
         <div class="col-md-12">
-                @if(count($projects) > 0)
-                <ul class="list-group m-0">
-                    @foreach ($projects as $project)
-                    <li class="list-group-item">
-                        <span>
-                            @if ($project->fk_type == 1) <i class="fas fa-lock"></i> @endif
-                            <a href="#">{{ $project->title }}</a> - {{ str_limit($project->description, $limit = 100, $end = '...') }}
-                        </span>
-                        <span class="float-right text-muted">{{ date('d.m.Y', strtotime($project->updated_at)) }}</span>
-                    </li>
-                    @endforeach
-                </ul>
-                {{ $projects->links() }}
+            @if(count($projects) > 0)
+            <ul class="list-group m-0">
+                @foreach ($projects as $project)
+                <li class="list-group-item">
+                    <span>
+                        @if ($project->fk_type == 1) <i class="fas fa-lock"></i> @endif
+                        <a href="#">{{ $project->title }}</a> -
+                        {{ str_limit($project->description, $limit = 100, $end = '...') }}
+                    </span>
+                    <span class="float-right text-muted">{{ date('d.m.Y', strtotime($project->updated_at)) }}</span>
+                </li>
+                @endforeach
+            </ul>
+            {{ $projects->links() }}
             @else
-                <p>Nemáte žádný projekt.</p>
+            <p>Nemáte žádný projekt.</p>
             @endif
         </div>
     </div>
