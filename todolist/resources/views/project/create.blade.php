41d5043cdda7eccb0e305fbab3f2b95811797b17
Gygram <40740085+Gygram@users.noreply.github.com>
2019-05-21 11:08:14 +0200

    Update create.blade.php

--- todolist/resources/views/project/create.blade.php
+++ todolist/resources/views/project/create.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
 <div class="container">
-    
+    <div class="col-md-12">
+            <div class="card">
+                    <div class="card-header">
+                      Featured
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">Special title treatment</h5>
+                      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+    </div>
 </div>
 @endsection
