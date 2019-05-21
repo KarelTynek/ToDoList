9940dbaa8b7d37676b678161d1c7f3f8321b346c
Gygram <40740085+Gygram@users.noreply.github.com>
2019-05-21 09:33:26 +0200

    Project model

--- todolist/app/Http/Controllers/UserController.php
+++ todolist/app/Http/Controllers/UserController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App;
 
 class UserController extends Controller
 {
+    public function profile() {
+        dd(Self::getUserProjects());
+    }
 
+    private function getUserProjects() {
+        return App\Project::select('title', 'created_at', 'updated_at', 'owner', 'id_project')
+            ->join('user_project', 'fk_project', '=', 'id_project')
+            ->where('fk_user', Auth::id())
+            ->get();
+    }
 }
--- todolist/app/Project.php
+++ todolist/app/Project.php
@@ -3,8 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Webpatser\Uuid\Uuid;
 
 class Project extends Model
 {
-    //
+    protected $primaryKey = 'id_project';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'title', 'description', 'password', 'fk_type',
+    ];
+
+    protected $hidden = [
+        'id_project', 'created_at', 'updated_at', 'owner',
+    ];
+
+    public static function boot() {
+        parent::boot();
+
+        self::creating(function($model){
+            $model->id = self::generateUuid();
+        });
+    }
+
+    public static function generateUuid() {
+        return Uuid::generate();
+    }
 }
