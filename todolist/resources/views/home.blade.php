bb247a2c8cec73c252686fa06ff7edfbec6a1848
Gygram <40740085+Gygram@users.noreply.github.com>
2019-05-21 09:41:37 +0200

    Design

--- todolist/resources/views/home.blade.php
+++ todolist/resources/views/home.blade.php
@@ -2,25 +2,31 @@
 
 @section('content')
 <div class="container">
-        <div class="card-columns" style="margin-top: 40px">
-                <div class="card">
-                    <div class="card-body  shadow-sm rounded">
-                            <h5 class="card-title text-center">Card title</h5>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Integer vulputate sem a nibh rutrum consequat. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam. Nullam sit amet magna in magna gravida vehicula. Integer vulputate sem a nibh rutrum consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Mauris elementum mauris vitae tortor. Fusce wisi.</p>
-                    </div>
+    <div class="row mt-2">
+        <div class="col-md-4 mb-1">
+            <div class="card">
+                <div class="card-body  shadow-sm rounded">
+                    <h5 class="card-title text-center">Card title</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Integer vulputate sem a nibh rutrum consequat. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam. Nullam sit amet magna in magna gravida vehicula. Integer vulputate sem a nibh rutrum consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Mauris elementum mauris vitae tortor. Fusce wisi.</p>
                 </div>
-                <div class="card">
-                    <div class="card-body shadow-sm rounded">
-                        <h5 class="card-title text-center">Card title</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Integer vulputate sem a nibh rutrum consequat. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus.</p>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="card-body shadow-sm rounded">
-                        <h5 class="card-title text-center">Card title</h5>
-                        <p class="card-text">Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam. Nullam sit amet magna in magna gravida vehicula. Integer vulputate sem a nibh rutrum consequat.</p>
-                    </div>
-                </div>
+            </div>
         </div>
+        <div class="col-md-4 mt-1 mb-1">
+            <div class="card">
+                <div class="card-body  shadow-sm rounded">
+                    <h5 class="card-title text-center">Card title</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Integer vulputate sem a nibh rutrum consequat. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam. Nullam sit amet magna in magna gravida vehicula. Integer vulputate sem a nibh rutrum consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Mauris elementum mauris vitae tortor. Fusce wisi.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mt-1">
+            <div class="card">
+                <div class="card-body  shadow-sm rounded">
+                    <h5 class="card-title text-center">Card title</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Integer vulputate sem a nibh rutrum consequat. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam. Nullam sit amet magna in magna gravida vehicula. Integer vulputate sem a nibh rutrum consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Mauris elementum mauris vitae tortor. Fusce wisi.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
