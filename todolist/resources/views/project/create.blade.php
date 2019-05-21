63f364a11f145ee35cd39002e98405f78bc6641d
Gygram <40740085+Gygram@users.noreply.github.com>
2019-05-21 11:24:01 +0200

    Update create.blade.php

--- todolist/resources/views/project/create.blade.php
+++ todolist/resources/views/project/create.blade.php
@@ -2,17 +2,35 @@
 
 @section('content')
 <div class="container">
+  <div class="row mt-2">
     <div class="col-md-12">
-            <div class="card">
-                    <div class="card-header">
-                      Featured
+      <div class="card">
+        <div class="card-header">
+          Vytvoření projektu
+        </div>
+        <div class="card-body">
+            <form method="post" action="">
+              <div class="form-group">
+                  <div class="input-group">
+                      <div class="input-group-prepend">
+                        <div class="input-group-text">Název</div>
+                      </div>
+                      <input name="title" type="text" class="form-control" max="32" />
                     </div>
-                    <div class="card-body">
-                      <h5 class="card-title">Special title treatment</h5>
-                      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+              <div class="form-group">
+                  <div class="input-group">
+                      <div class="input-group-prepend">
+                        <div class="input-group-text">Popisek</div>
+                      </div>
+                      <textarea class="form-control" ></textarea>
                     </div>
-                  </div>
+              </div>
+
+            </form>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
 @endsection
