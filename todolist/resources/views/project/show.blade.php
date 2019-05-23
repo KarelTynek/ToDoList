99ec1a4a380ef8f9fdd47d99a1b1713af1f507b3
KarelTynek <50730505+KarelTynek@users.noreply.github.com>
2019-05-23 07:48:18 +0200

    Modal for delete

--- todolist/resources/views/project/show.blade.php
+++ todolist/resources/views/project/show.blade.php
@@ -25,7 +25,7 @@
                <div class="float-right">
                   <i class="fas fa-plus mr-1"></i>
                   <i class="fas fa-pen mr-1"></i>
-                  <i class="fas fa-trash-alt"></i>
+                  <i class="fas fa-trash-alt" data-toggle="modal" data-target="#exampleModalLong"></i>
                </div>
             </div>
             <div class="card-body text-secondary"></div>
@@ -35,6 +35,28 @@
    </div>
    @endforeach
 </div>
+
+<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+   <div class="modal-dialog" role="document">
+     <div class="modal-content">
+       <div class="modal-header">
+         <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+           <span aria-hidden="true">&times;</span>
+         </button>
+       </div>
+       <div class="modal-body">
+         ...
+       </div>
+       <div class="modal-footer">
+         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+         <button type="button" class="btn btn-primary">Save changes</button>
+       </div>
+     </div>
+   </div>
+ </div>
+
+
 
 <div class="modal fade" id="columnmodal" tabindex="-1" role="dialog" aria-labelledby="columnmodallabel"
    aria-hidden="true">
