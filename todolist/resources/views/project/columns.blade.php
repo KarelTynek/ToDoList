6bdf6c6e0701c973fd86b3c5de718565b887ea53
Gygram <40740085+Gygram@users.noreply.github.com>
2019-05-24 10:00:05 +0200

    Columns design

--- todolist/resources/views/project/columns.blade.php
+++ todolist/resources/views/project/columns.blade.php
@@ -1,8 +1,8 @@
 @foreach ($columns->chunk(4) as $chunk)
 <div class="row">
    @foreach ($chunk as $item)
-   <div class="col-md-3 mb-3 d-flex">
-      <div class="card shadow-sm w-100">
+   <div class="col-md-3 mb-3">
+      <div class="card shadow-sm column">
          <div class="card-header">
             {{ $item->name }}
             <div class="float-right">
@@ -11,7 +11,7 @@
                <i class="fas fa-trash-alt"></i>
             </div>
          </div>
-         <div class="card-body">
+         <div class="card-body column-text">
             <input type="hidden" name="id" value="{{ $item->id_column }}" />
             @foreach ($rows as $row)
                @if ($row->fk_column == $item->id_column)
