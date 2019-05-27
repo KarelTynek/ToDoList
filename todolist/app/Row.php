00b13b2fd17bf25a677c58b9e046937c904ca72e
Gygram <40740085+Gygram@users.noreply.github.com>
2019-05-27 10:41:44 +0200

    Update Row.php

--- todolist/app/Row.php
+++ todolist/app/Row.php
@@ -17,7 +17,7 @@ class Row extends Model
     ];
 
     public static function getRows($id) {
-        return Self::select('rows.description', 'fk_column')
+        return Self::select('rows.description', 'fk_column', 'id_row')
             ->join('columns', 'id_column', '=', 'fk_column')
             ->join('projects', 'id_project', '=', 'fk_project')
             ->where('id_project', $id)
