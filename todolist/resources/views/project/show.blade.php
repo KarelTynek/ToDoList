9c37f6aa0b8e40420d8153203fefc335999d9a53
KarelTynek <50730505+KarelTynek@users.noreply.github.com>
2019-05-27 11:59:27 +0200

    Update show.blade.php

--- todolist/resources/views/project/show.blade.php
+++ todolist/resources/views/project/show.blade.php
@@ -59,7 +59,14 @@
 
 @push('scripts')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script>
+
+$( function() {
+    $( "#test" ).draggable({ revert: true });
+  } );
+
+
    /**
  * CRUD ACTIONS
 */
@@ -285,5 +292,8 @@ $('#addcolumn').click(function() {
          console.log(request.responseText);
       });
    }
+
+   
+
 </script>
 @endpush
