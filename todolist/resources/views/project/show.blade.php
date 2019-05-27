5a3f756e7ce54e020318faaab42193470fd9d73e
KarelTynek <50730505+KarelTynek@users.noreply.github.com>
2019-05-27 12:29:25 +0200

    Update show.blade.php

--- todolist/resources/views/project/show.blade.php
+++ todolist/resources/views/project/show.blade.php
@@ -61,10 +61,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
 <script>
-$( ".draggable" ).draggable({
-   appendTo: "body"
-});
-
    /**
  * CRUD ACTIONS
 */
