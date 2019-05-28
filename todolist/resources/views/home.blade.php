a6a282362e7bbc5d2f76be67db7d7da5641a6975
KarelTynek <50730505+KarelTynek@users.noreply.github.com>
2019-05-28 08:24:37 +0200

    Update home.blade.php

--- todolist/resources/views/home.blade.php
+++ todolist/resources/views/home.blade.php
@@ -2,26 +2,28 @@
 
 @section('content')
 
-        
+
 <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-          <h1 class="display-4">Todolist</h1>
-          <p class="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam erat volutpat. Integer pellentesque quam vel velit. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus.</p>
-        </div>
-      </div>
-
+    <div class="container">
+        <h1 class="display-4">Todolist</h1>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam erat volutpat. Integer
+            pellentesque quam vel velit. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Duis bibendum,
+            lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus.</p>
+    </div>
+</div>
 <div class="container">
-    
-        
-            <hr>
-
-
     <div class="row mt-2">
         <div class="col-md-4 mb-1">
             <div class="card">
                 <div class="card-body  shadow-sm rounded">
                     <h5 class="card-title text-center">Card title</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Integer vulputate sem a nibh rutrum consequat. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam. Nullam sit amet magna in magna gravida vehicula. Integer vulputate sem a nibh rutrum consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Mauris elementum mauris vitae tortor. Fusce wisi.</p>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam lectus justo,
+                        vulputate eget mollis sed, tempor sed magna. Nulla turpis magna, cursus sit amet, suscipit a,
+                        interdum id, felis. Integer vulputate sem a nibh rutrum consequat. Mauris dolor felis, sagittis
+                        at, luctus sed, aliquam non, tellus. Pellentesque pretium lectus id turpis. Etiam dictum
+                        tincidunt diam. Nullam sit amet magna in magna gravida vehicula. Integer vulputate sem a nibh
+                        rutrum consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+                        eu fugiat nulla pariatur. Mauris elementum mauris vitae tortor. Fusce wisi.</p>
                 </div>
             </div>
         </div>
@@ -29,7 +31,13 @@
             <div class="card">
                 <div class="card-body  shadow-sm rounded">
                     <h5 class="card-title text-center">Card title</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Integer vulputate sem a nibh rutrum consequat. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam. Nullam sit amet magna in magna gravida vehicula. Integer vulputate sem a nibh rutrum consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Mauris elementum mauris vitae tortor. Fusce wisi.</p>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam lectus justo,
+                        vulputate eget mollis sed, tempor sed magna. Nulla turpis magna, cursus sit amet, suscipit a,
+                        interdum id, felis. Integer vulputate sem a nibh rutrum consequat. Mauris dolor felis, sagittis
+                        at, luctus sed, aliquam non, tellus. Pellentesque pretium lectus id turpis. Etiam dictum
+                        tincidunt diam. Nullam sit amet magna in magna gravida vehicula. Integer vulputate sem a nibh
+                        rutrum consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+                        eu fugiat nulla pariatur. Mauris elementum mauris vitae tortor. Fusce wisi.</p>
                 </div>
             </div>
         </div>
@@ -37,11 +45,17 @@
             <div class="card">
                 <div class="card-body  shadow-sm rounded">
                     <h5 class="card-title text-center">Card title</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Integer vulputate sem a nibh rutrum consequat. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam. Nullam sit amet magna in magna gravida vehicula. Integer vulputate sem a nibh rutrum consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Mauris elementum mauris vitae tortor. Fusce wisi.</p>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam lectus justo,
+                        vulputate eget mollis sed, tempor sed magna. Nulla turpis magna, cursus sit amet, suscipit a,
+                        interdum id, felis. Integer vulputate sem a nibh rutrum consequat. Mauris dolor felis, sagittis
+                        at, luctus sed, aliquam non, tellus. Pellentesque pretium lectus id turpis. Etiam dictum
+                        tincidunt diam. Nullam sit amet magna in magna gravida vehicula. Integer vulputate sem a nibh
+                        rutrum consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+                        eu fugiat nulla pariatur. Mauris elementum mauris vitae tortor. Fusce wisi.</p>
                 </div>
             </div>
         </div>
     </div>
-    
+
 </div>
 @endsection
