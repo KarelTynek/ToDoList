68c3eae2db206a9d8e553cb919ce8629bc26a4c5
Gygram <40740085+Gygram@users.noreply.github.com>
2019-05-29 10:43:57 +0200

    Update show.blade.php

--- todolist/resources/views/project/show.blade.php
+++ todolist/resources/views/project/show.blade.php
@@ -42,6 +42,28 @@
    </div>
    <hr />
    <div id="columnscontainer">
+         <div id="disqus_thread"></div>
+         <script>
+         
+         /**
+         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+         /*
+         var disqus_config = function () {
+         this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+         this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+         };
+         */
+         (function() { // DON'T EDIT BELOW THIS LINE
+         var d = document, s = d.createElement('script');
+         s.src = 'https://zeroscans-1.disqus.com/embed.js';
+         s.setAttribute('data-timestamp', +new Date());
+         (d.head || d.body).appendChild(s);
+         })();
+         </script>
+         <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                                 
+         <a href="http://foo.com/bar.html#disqus_thread">Link</a>
       @include('project.columns')
    </div>
 </div>
@@ -163,6 +185,7 @@
 
 @push('scripts')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script id="dsq-count-scr" src="//zeroscans-1.disqus.com/count.js" async></script>
 <script>
 
 function addForm(column) {
